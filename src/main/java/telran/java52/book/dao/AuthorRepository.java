aaa6c85d5de483d1d322ee4d6c634ca6fe38d1f1
Larisa <168708413+Larawrx@users.noreply.github.com>
2024-06-15 13:11:21 +0300

    EntityManager firs commit

--- src/main/java/telran/java52/book/dao/AuthorRepository.java
+++ src/main/java/telran/java52/book/dao/AuthorRepository.java
@@ -1,9 +1,20 @@
 package telran.java52.book.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import telran.java52.book.model.Author;
+import telran.java52.book.model.Publisher;
 
-public interface AuthorRepository extends JpaRepository<Author, String> {
+public interface AuthorRepository  {
+	
+	Optional<Author> findById(String authorName);
+	
+	Author save (Author author);
+
+	void deleteById(String authorName);
+
+	
 
 }
