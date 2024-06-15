2e6471f10173749f1320010a79b56c1c6fed6653
Larisa <168708413+Larawrx@users.noreply.github.com>
2024-06-15 11:03:31 +0300

    l

--- src/main/java/telran/java52/book/model/Author.java
+++ src/main/java/telran/java52/book/model/Author.java
@@ -2,9 +2,11 @@ package telran.java52.book.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,4 +26,14 @@ public class Author implements Serializable{
 	@Id
 	String name;
     LocalDate birthDate;
+    @ManyToMany(mappedBy = "authors")//всегда ставится на строне родительской сущьности
+    Set<Book> books;
+	
+    public Author(String name, LocalDate birthDate) {
+		super();
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+    
+    
 }
--- src/main/java/telran/java52/book/model/Publisher.java
+++ src/main/java/telran/java52/book/model/Publisher.java
@@ -1,9 +1,11 @@
 package telran.java52.book.model;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,9 +24,20 @@ public class Publisher implements Serializable{
 	
 	@Id
 	String publisherName;
+	@OneToMany(mappedBy = "publisher")//аннотация говорит от какого поле зависит
+	Set<Book> books;
+	
+	public Publisher(String publisherName) {
+		super();
+		this.publisherName = publisherName;
+	}
 	
 	@Override
 	public String toString() {
 		return publisherName;
 	}
+
+
+
+	
 }
